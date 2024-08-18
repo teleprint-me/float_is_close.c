2983824dce6254e1a36139c13aa91c30bed69c0a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-18 17:06:59 -0400

    float_is_close(python): Add initial outline for custom test suite

--- float_is_close.py
+++ float_is_close.py
@@ -15,7 +15,8 @@ IMPORTANT: The 53-bit significand precision gives 15 to 17 significant decimal
            digits of precision (2e-53 ≈ 1.11 x 10e-16).
 """
 
-from math import isinf, isnan
+from dataclasses import dataclass
+from math import inf, isinf, isnan, nan
 
 
 def float_is_close(
@@ -44,8 +45,52 @@ def float_is_close(
     return difference <= tolerance
 
 
+@dataclass
+class TestCase:
+    a: float
+    b: float
+    significand: int
+    expected: bool
+
+
+def fixture_float_is_close() -> list[TestCase]:
+    # Arbitrary values used for testing
+    n: float = 0.053803  # exclude the number while including the fraction
+    m: float = 123456789.123456  # include number and fraction
+    o: float = 0.053721  # less than n
+    p: float = 0.053951  # greater than n
+
+    return [
+        # precision is within a range of 6 digits
+        TestCase(0.053803, n, 6, True),  # equal to
+        TestCase(n, o, 6, False),  # less than
+        TestCase(n, p, 6, False),  # greater than
+        # Negative numbers
+        TestCase(-0.053803, -n, 6, True),  # equal to
+        TestCase(-n, -o, 6, False),  # greater than
+        TestCase(-n, -p, 6, False),  # less than
+        # Inf and NaN cases
+        TestCase(inf, inf, 6, True),  # INF is equal to itself
+        TestCase(nan, 0.0, 6, False),  # NaN is not equal to any object
+        TestCase(nan, nan, 6, False),  # NaN is not equal to itself
+        # Large numbers with significant precision
+        TestCase(1e6, 1e6 + 1, 0, True),  # no precision
+        TestCase(1e-6, 1e-6 + 1e-9, 6, True),  # Very close but small tolerance
+        # Test large significand
+        TestCase(123456789.123456, m, 15, True),
+        # Test differentiated large significand
+        TestCase(m, 123456789.123457, 15, False),
+        # Small numbers with large tolerance
+        TestCase(1e-15, 2e-15, 15, False),
+    ]
+
+
+def test_float_is_close() -> bool:
+    """
+    Returns True if tests pass, False if any test fails completion
+    """
+    pass
+
+
 if __name__ == "__main__":
-    result = float_is_close(1e-15, 2e-15, 15)
-    print(
-        result is False
-    )  # Expected output: True (since the values are not close)
+    test_float_is_close()
