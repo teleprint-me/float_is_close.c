68a83d8863a123b96da5f00c6be29f286164cb63
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-18 14:39:07 -0400

    python(feat): Add proof of concept

---
+++ float_is_close.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# NOTE: Requires python 3.7 or greater
+
+# NOTE: This is a minimalistic dependency.
+#
+# If tests, or further extentions are to be implemented,
+# then there should be no other dependency than the builtin standard library.
+
+# Proof of concept
+from math import isinf, isnan
+
+
+def float_is_close(a: float, b: float, significand: int, relative_tolerance: float = 1e-9) -> bool:
+    # Numbers are equal
+    if a == b:
+        return True
+
+    # Arguments are not numbers
+    if isinf(a) or isinf(b) or isnan(a) or isnan(b):
+        return False
+
+    absolute_tolerance = 10**-significand
+    tolerance = max(relative_tolerance * max(abs(a), abs(b)), absolute_tolerance)
+    diff = abs(a - b)
+
+    return diff <= tolerance
+
+
+# 1e-15 is not equal to 2e-15
+result = float_is_close(1e-15, 2e-15, 15)
+print(result is False)  # NOTE: This resolves to False
