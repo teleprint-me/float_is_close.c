2b896b84b131173afb5a02ec7f5deb8f512527f5
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-18 16:42:16 -0400

    float_is_close(python): Clean up, ensure significand is non-zero and positive, and enhance docstring and relevant comments

--- float_is_close.py
+++ float_is_close.py
@@ -5,42 +5,47 @@ Copyright © 2024 Austin Berrio
 
 float_is_close.py
 
-A simple proof of concept for float_is_close.c in pure python
+Proof of concept for float_is_close.c in pure Python.
 
-NOTE: Requires python 3.7 or greater
+Requires Python 3.7 or greater.
 
-NOTE: This is a minimalistic dependency.
+This script is minimalistic with no dependencies beyond the standard library.
 
-NOTE: If tests, or further extentions are to be implemented,
-      then there should be no other dependency than the builtin
-      standard library.
+IMPORTANT: The 53-bit significand precision gives 15 to 17 significant decimal
+           digits of precision (2e-53 ≈ 1.11 x 10e-16).
 """
 
-# Proof of concept
 from math import isinf, isnan
 
 
 def float_is_close(
-    a: float, b: float, significand: int, relative_tolerance: float = 1e-9
+    a: float, b: float, significand: int, relative_tolerance: float = 1e-15
 ) -> bool:
-    # Numbers are equal
     if a == b:
         return True
 
-    # Arguments are not numbers
     if isinf(a) or isinf(b) or isnan(a) or isnan(b):
         return False
 
+    # Ensure significand is positive and non-zero
+    significand = max(1, abs(significand))
+
+    # Calculate the absolute tolerance as a power of 10
     absolute_tolerance = 10**-significand
+
+    # Compute the tolerance considering both relative and absolute tolerance
     tolerance = max(
         relative_tolerance * max(abs(a), abs(b)), absolute_tolerance
     )
-    diff = abs(a - b)
 
-    return diff <= tolerance
+    # Calculate the absolute difference between the input values
+    difference = abs(a - b)
+
+    return difference <= tolerance
 
 
 if __name__ == "__main__":
-    # 1e-15 is not equal to 2e-15
     result = float_is_close(1e-15, 2e-15, 15)
-    print(result is False)  # NOTE: This resolves to False
+    print(
+        result is False
+    )  # Expected output: True (since the values are not close)
